9ca04c2fbd919248eb73073e1d6968c7cd8cef7f
Bytebarian <bytebarian@gmail.com>
2017-12-15 14:33:49 +0100

    codebase

---
+++ PedestrianTracker/PedestrianTracker.Core/VideoCaptureService.cs
@@ -0,0 +1,20 @@
+﻿using Emgu.CV;
+
+namespace PedestrianTracker.Core
+{
+    public class VideoCaptureService
+    {
+        Capture _capture;
+
+        public VideoCaptureService()
+        {
+            _capture = new Capture("test.avi");
+        }
+
+        public Image<Emgu.CV.Structure.Bgr, byte> GetNextFrame()
+        {
+            var frame = _capture.QueryFrame();
+            return frame;
+        }
+    }
+}
